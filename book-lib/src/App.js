7dc0e6e465fb6e57835883b5db34050e0369bda5
Ockbar <afoysal0422@gmail.com>
2023-12-10 15:33:15 -0500

    Search Component Added and Attempted Backend Link

--- book-lib/src/App.js
+++ book-lib/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import SearchBooks from "./components/Search";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/search" element={<SearchBooks />} />
+      </Routes>
     </>
   );
 }
