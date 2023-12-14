c733976e813e54702dd6462f8d26cba0456df982
Ockbar <afoysal0422@gmail.com>
2023-12-14 18:37:42 -0500

    Added Google API Search Functionality

---
+++ book-lib/src/components/Search/BookCard.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import BookModel from "./BookModel";
+const BookCard = ({ book }) => {
+	const [show, setShow] = useState(false);
+	const [bookItem, setItem] = useState();
+	console.log(book);
+	return (
+		<>
+			{book.map((item) => {
+				let thumbnail =
+					item.volumeInfo.imageLinks &&
+					item.volumeInfo.imageLinks.smallThumbnail;
+				if (thumbnail !== undefined) {
+					return (
+						<>
+							<div
+								className='BookCard'
+								onClick={() => {
+									setShow(true);
+									setItem(item);
+								}}
+							>
+								<img src={thumbnail} alt='' />
+								<div className='bottom'>
+									<h3 className='title'>
+										{item.volumeInfo.title}
+									</h3>
+								</div>
+							</div>
+							<BookModel
+								show={show}
+								item={bookItem}
+								onClose={() => setShow(false)}
+							/>
+						</>
+					);
+				}
+			})}
+		</>
+	);
+};
+export default BookCard;
